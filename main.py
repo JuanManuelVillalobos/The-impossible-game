2ddf9c0e2dc8e4c8515a10ac8d34dbbb9f15a97f
a01639510 <a01639510@tec.mx>
2023-10-17 10:38:51 -0600

    highscore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from classes import Player, Level1, Level2
+from classes import Player, Level1, Level2, HighScore
 
 BACKGROUND = (0, 154, 255)
 
@@ -8,10 +8,12 @@ SCREEN_WIDTH = 750
 SCREEN_HEIGHT = 450
 
 # Create a 1280x720 sized screen
+high_score_manager = HighScore()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 # Set the title of the window
 pygame.display.set_caption("The Impossible Game")
+
 
 
 # Handle movement of enemies
@@ -31,6 +33,7 @@ levels = [Level1(), Level2()]
 player = Player(70, 220)
 
 clock = pygame.time.Clock()
+n = 0
 
 for level in range(len(levels)):
     player.respawn.x = levels[level].playerRespawn.x
@@ -44,13 +47,19 @@ for level in range(len(levels)):
     done = False
     player.win = False
     while not done:
-
+        
+        
+        n = n + 10
+        
+        pygame.display.update()
 
         if player.win == True:
             break
 
 
         for event in pygame.event.get():
+
+            
             if event.type == pygame.QUIT:
                 done = True
 
@@ -83,16 +92,34 @@ for level in range(len(levels)):
         all_sprite_list.draw(screen)
 
         pygame.display.flip()
+    
 
         player.winner()
 
         clock.tick(60)
+        
 
     all_sprite_list.remove(levels[level].level_sprite_list)
     all_sprite_list.remove(player)
-
+    
     if done:
         pygame.quit()
         break
-HighScore.update()
+
+BACKGROUND = (0, 154, 255)
+
+# Screen dimensions
+SCREEN_WIDTH = 550
+SCREEN_HEIGHT = 250
+
+# Create a 1280x720 sized screen
+high_score_manager = HighScore()
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+# Set the title of the window
+pygame.display.set_caption("")
+new_score = 9999 - n
+high_score_manager.update_high_scores(new_score)
+high_score_manager.display_high_scores(screen)  # Draw high scores after updating the display
+
 pygame.quit()
